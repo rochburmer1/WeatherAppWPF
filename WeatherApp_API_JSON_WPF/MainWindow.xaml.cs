4d4837961b05d853e5d488d922846706882dd8d5
rochburmer1 <roch.burmer@microsoft.wsei.edu.pl>
2025-08-08 20:14:42 +0200

    icons, api, no tests

---
+++ WeatherApp_API_JSON_WPF/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WeatherApp
+{
+    public partial class MainWindow : Window
+    {
+        private const string API_KEY = "9a9ce366878938edee44cdab1b0a6dc1";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetWeather_Click(object sender, RoutedEventArgs e)
+        {
+            string city = CityTextBox.Text;
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&lang=pl";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string response = await client.GetStringAsync(url);
+                    JObject weatherData = JObject.Parse(response);
+
+                    double temp = (double)weatherData["main"]["temp"];
+                    int pressure = (int)weatherData["main"]["pressure"];
+                    int humidity = (int)weatherData["main"]["humidity"];
+                    string description = weatherData["weather"][0]["description"].ToString();
+                    string iconCode = weatherData["weather"][0]["icon"].ToString();
+
+                    TemperatureText.Text = $"Temperatura: {temp}°C";
+                    PressureText.Text = $"Ciśnienie: {pressure} hPa";
+                    HumidityText.Text = $"Wilgotność: {humidity}%";
+                    DescriptionText.Text = description;
+
+                    WeatherIcon.Source = new BitmapImage(new Uri($"http://openweathermap.org/img/wn/{iconCode}@2x.png"));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Błąd: " + ex.Message);
+                }
+            }
+        }
+    }
+}
